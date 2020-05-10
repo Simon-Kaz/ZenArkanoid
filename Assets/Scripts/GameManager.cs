d6ce36136cf7586e4043031bf566a2f323be5626
Szymon Kazmierczak <skazmierczak@zendesk.com>
2020-05-10 20:46:48 +0100

    main menu, ball prefab for life  management, game manager early stages

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public bool isGameActive;
+    public int score;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI restartButton;
+    public TextMeshProUGUI scoreText;
+    public GameObject titleScreen;
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+
+        SpawnBall();
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    // Create a new ball that immediately starts moving
+    public void SpawnBall()
+    {
+        throw new NullReferenceException();
+        // generate new instance of ball game object
+        // add velocity
+    }
 
 }
- 
